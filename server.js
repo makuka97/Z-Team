cee0e5faea60240b24e4f9fbc18928f9ece82913
Micajah Nordyke <mnordyk1@my.chemeketa.edu>
2026-03-18 23:41:38 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+{
+  "name": "z-team-pc",
+  "version": "1.0.0",
+  "description": "Z-TEAM PC Multiplayer Edition",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "socket.io": "^4.7.2"
+  },
+  "engines": {
+    "node": ">=18"
+  }
+}
